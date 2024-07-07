b6e44ae07efd1ec20b42b40d135ec6c7eb253c95
kjw <kingcoding2298@gmail.com>
2024-07-07 15:28:29 +0900

    playerName 버튼 클릭 이벤트 적용
    버튼 클릭시 Edit -> Save 번갈아가면서 설정됨

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,42 @@
+import {useState} from "react";
+
 export default function Player ({name, symbol}) {
+    const [buttonValue, setButton] = useState();
+    let isButton = true
+    let playerName =  <span className="player-name">{name}</span>;
+    let buttonType = <button onClick={() => handleButton("edit")}>Edit</button>
+  
+
+
+    function handleButton (isButton){
+
+        console.log(isButton);
+        
+        if(isButton == "edit"){
+          setButton(true);
+        }else{
+          setButton(false);
+        }
+       
+    }
+
+
+    if(buttonValue){
+    
+      playerName = <input type="text" ></input>
+      buttonType = <button onClick={() =>handleButton("save")}>Save</button>
+   
+    }
+    
+
+
     return(
         <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        {buttonType}
       </li>
     );
 }
