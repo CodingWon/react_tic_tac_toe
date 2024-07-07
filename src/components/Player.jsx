e3d059f8fe5f78f2858eb01dc261170521e2f110
kjw <kingcoding2298@gmail.com>
2024-07-07 15:33:13 +0900

    버튼 클릭시 이벤트_74_개념복습_State(상태)_활용법
    버튼 클릭시 inputBox 적용

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import {useState} from "react";
+
 export default function Player ({name, symbol}) {
+    const [isEditing, setIsEditing] = useState();
+ 
+    let playerName =  <span className="player-name">{name}</span>;
+  
+    function handleButton (){
+      setIsEditing(true);
+    }
+
+
+    if(isEditing){
+      playerName = <input type="text" required ></input>
+   
+    }
+
     return(
         <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+          <button onClick={handleButton}>Edit</button>
       </li>
     );
 }
