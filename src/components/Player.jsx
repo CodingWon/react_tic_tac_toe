e90cf36ad05cbfec835596b60b965f72a0f13f2d
kjw <kingcoding2298@gmail.com>
2024-07-07 20:42:50 +0900

    81. State(상태) 끌어올리기 [핵심 개념]_player 바꾸기
    - O,X 플레이어 바꾸기

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -8,7 +8,7 @@ export default function Player({ initialName, symbol }) {
     setIsEditing((editing) => !editing);
 
   }
-  
+
   function handleChange(e) {
     console.log(e.target.value);
     let editName = e.target.value
@@ -21,7 +21,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
