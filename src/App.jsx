2dbe7eb35f9acd6aaf22dcc03425886b00284261
kjw <kingcoding2298@gmail.com>
2024-07-08 15:24:40 +0900

    82_교차 State(상태) 방지하기_Log컴포넌트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react"
 
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);         // 버튼 하나를 클릭할 때마다 이 배열에 순서를 하니씩 추가한다. 
+                                                          //상태를 하나 더 만들어서 크게 다를 것 없는 정보를 저장하는 것은 리액트 개발자로서 피하는 것이 좋다.
 
-  function handleSelectSquare() {
+  function handleSelectSquare() {                         // GameBoard 에서 handleSelectSqure 를 제어한다.
     setActivePlayer((curActivePlayer) => {
-      const result = curActivePlayer === 'X' ? 'O' : 'X'
-      return result;
-    });
+      curActivePlayer === 'X' ? 'O' : 'X'
+      setGameTurns();
+    }
+    );
   }
 
   return (
@@ -24,7 +27,7 @@ function App() {
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol = {activePlayer} />
       </div>
 
-      {/*LOG*/}
+     <Log />
     </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,19 +6,19 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-
+// 여기서는 게임판을 업데이터하되 매 클릭마다 업데이트가 실행된다. 어떤 순서로 클릭했는지는 알 수 없지만 위치로는 알 수 있다.
 export default function GameBoard({onSelectSquare , activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
 
-        onSelectSquare();
-    }
+    //     onSelectSquare();
+    // }
 
 
     return (<ol id="game-board">
